426b3e50941d12cc522188fa6787817f8005e9a7
Muhammadamin <79371764+muhammadamin1308@users.noreply.github.com>
2024-09-04 19:03:54 +0500

    untill 36 section

--- 33. PostgreSQL/8.5 Family Travel Tracker/index.js
+++ 33. PostgreSQL/8.5 Family Travel Tracker/index.js
@@ -1,18 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-
+ 
 const app = express();
 const port = 3000;
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "world",
-  password: "0211",
-  port: 5432,
-});
-db.connect();
+  const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "world",
+    password: "0211",
+    port: 5432,
+  });
+  db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
